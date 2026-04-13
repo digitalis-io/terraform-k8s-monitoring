afa98722020b69ca39d84ab5e6499a395b422cb9
Sergio Rua <sergio.rua@digitalis.io>
2026-04-13 18:55:30 +0100

    feat(prometheus): split ingress into grafana, prometheus, and alertmanager
    Replace the single ingress_* fields with three sub-objects:
      grafana_ingress, prometheus_ingress, alertmanager_ingress
    Each has: enabled, host, class_name, tls_secret, annotations.
    tls_secret auto-derived per component when empty.
    Separate validations enforce host is set when enabled is true.

--- modules/prometheus/main.tf
+++ modules/prometheus/main.tf
@@ -34,12 +34,25 @@ resource "helm_release" "prometheus" {
       storage_class        = var.prometheus.storage_class
       grafana_enabled      = var.prometheus.grafana_enabled
       alertmanager_enabled = var.prometheus.alertmanager_enabled
-      ingress_enabled      = var.prometheus.ingress_enabled
-      ingress_host         = var.prometheus.ingress_host
-      ingress_class_name   = var.prometheus.ingress_class_name
-      ingress_tls_secret   = var.prometheus.ingress_tls_secret != "" ? var.prometheus.ingress_tls_secret : "${var.prometheus.namespace}-grafana-tls"
-      ingress_annotations  = var.prometheus.ingress_annotations
       namespace            = var.prometheus.namespace
+
+      grafana_ingress_enabled     = var.prometheus.grafana_ingress.enabled
+      grafana_ingress_host        = var.prometheus.grafana_ingress.host
+      grafana_ingress_class_name  = var.prometheus.grafana_ingress.class_name
+      grafana_ingress_tls_secret  = var.prometheus.grafana_ingress.tls_secret != "" ? var.prometheus.grafana_ingress.tls_secret : "${var.prometheus.namespace}-grafana-tls"
+      grafana_ingress_annotations = var.prometheus.grafana_ingress.annotations
+
+      prometheus_ingress_enabled     = var.prometheus.prometheus_ingress.enabled
+      prometheus_ingress_host        = var.prometheus.prometheus_ingress.host
+      prometheus_ingress_class_name  = var.prometheus.prometheus_ingress.class_name
+      prometheus_ingress_tls_secret  = var.prometheus.prometheus_ingress.tls_secret != "" ? var.prometheus.prometheus_ingress.tls_secret : "${var.prometheus.namespace}-prometheus-tls"
+      prometheus_ingress_annotations = var.prometheus.prometheus_ingress.annotations
+
+      alertmanager_ingress_enabled     = var.prometheus.alertmanager_ingress.enabled
+      alertmanager_ingress_host        = var.prometheus.alertmanager_ingress.host
+      alertmanager_ingress_class_name  = var.prometheus.alertmanager_ingress.class_name
+      alertmanager_ingress_tls_secret  = var.prometheus.alertmanager_ingress.tls_secret != "" ? var.prometheus.alertmanager_ingress.tls_secret : "${var.prometheus.namespace}-alertmanager-tls"
+      alertmanager_ingress_annotations = var.prometheus.alertmanager_ingress.annotations
 
       mimir_remote_write_url = var.prometheus.mimir_remote_write_url
       mimir_datasource_url   = var.prometheus.mimir_datasource_url
--- modules/prometheus/variables.tf
+++ modules/prometheus/variables.tf
@@ -6,13 +6,35 @@ variable "prometheus" {
     create_namespace     = optional(bool, true)
     grafana_enabled      = optional(bool, true)
     alertmanager_enabled = optional(bool, true)
-    ingress_enabled      = optional(bool, false)
-    ingress_host         = optional(string, "")
-    ingress_class_name   = optional(string, "nginx")
-    ingress_tls_secret   = optional(string, "")
-    ingress_annotations  = optional(map(string), {
-      "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
-    })
+    grafana_ingress = optional(object({
+      enabled     = optional(bool, false)
+      host        = optional(string, "")
+      class_name  = optional(string, "traefik")
+      tls_secret  = optional(string, "")
+      annotations = optional(map(string), {
+        "cert-manager.io/cluster-issuer" = "selfsigned-cluster-issuer"
+      })
+    }), {})
+
+    prometheus_ingress = optional(object({
+      enabled     = optional(bool, false)
+      host        = optional(string, "")
+      class_name  = optional(string, "traefik")
+      tls_secret  = optional(string, "")
+      annotations = optional(map(string), {
+        "cert-manager.io/cluster-issuer" = "selfsigned-cluster-issuer"
+      })
+    }), {})
+
+    alertmanager_ingress = optional(object({
+      enabled     = optional(bool, false)
+      host        = optional(string, "")
+      class_name  = optional(string, "traefik")
+      tls_secret  = optional(string, "")
+      annotations = optional(map(string), {
+        "cert-manager.io/cluster-issuer" = "selfsigned-cluster-issuer"
+      })
+    }), {})
     storage_size   = optional(string, "20Gi")
     storage_class  = optional(string, "")
     retention      = optional(string, "24h")
@@ -31,7 +53,17 @@ variable "prometheus" {
   default = {}
 
   validation {
-    condition     = !(var.prometheus.ingress_enabled && var.prometheus.ingress_host == "")
-    error_message = "ingress_host is required when ingress_enabled is true."
+    condition     = !(var.prometheus.grafana_ingress.enabled && var.prometheus.grafana_ingress.host == "")
+    error_message = "grafana_ingress.host is required when grafana_ingress.enabled is true."
+  }
+
+  validation {
+    condition     = !(var.prometheus.prometheus_ingress.enabled && var.prometheus.prometheus_ingress.host == "")
+    error_message = "prometheus_ingress.host is required when prometheus_ingress.enabled is true."
+  }
+
+  validation {
+    condition     = !(var.prometheus.alertmanager_ingress.enabled && var.prometheus.alertmanager_ingress.host == "")
+    error_message = "alertmanager_ingress.host is required when alertmanager_ingress.enabled is true."
   }
 }
